300508f170d0d535cecf28f8907ea81cdedf42ec
Mr_MSDV <37757663+mrmsdv@users.noreply.github.com>
2018-07-07 05:51:23 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
 echo "Please wait...."
+pkg install curl
 apt install unzip
 pkg install python2
 pkg install nano
 pkg install unzip
-unzip brigadetools.zip
+unzip brigadetoolsz.zip
 git clone https://github.com/sqlmapproject/sqlmap.git
 echo "Success Install..."
 echo "Tingaal Pakai Brothers.. ^_^"
